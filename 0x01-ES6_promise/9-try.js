74f26b11825cfd9bd8b220e9e973237084ccb6ea
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-06 21:24:35 +0000

    Added task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result, 'Guardrail was processed');
+  } catch (err) {
+    queue.push(err.message, 'Guardrail was processed');
+  }
+
+  return queue;
+}
