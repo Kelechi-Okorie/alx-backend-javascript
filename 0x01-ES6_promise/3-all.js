09f6b8d3c8742911dec5cf51a6ef7f26e71ce9eb
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-06 15:23:27 +0000

    Minor bug fix on task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2]).then((values) => {
+  return Promise.all([promise1, promise2]).then((values) => {
     const [result1, result2] = values;
     console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
   }).catch((err) => console.log('Signup system offline', err));
