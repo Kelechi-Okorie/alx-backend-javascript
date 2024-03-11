8dc7f00d9df29649587e4bab6178dc4badb553fd
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-11 18:18:16 +0000

    Bug fix on task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,7 +3,5 @@ export default function getListStudentIds(studentsList) {
     return [];
   }
 
-  return studentsList.map((student) => {
-    return student.id;
-  });
+  return studentsList.map((student) => student.id);
 }
